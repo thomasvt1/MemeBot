94af3e945641373cff756c5527a6656e87589a94
Keanu73 <guestone63@gmail.com>
2019-07-04 07:41:27 +0100

    Changed up scripts

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker rm memebot
 docker run -d --name memebot thomasvt1/memebot:latest
 docker cp configdata/config.js memebot:/home/user/config.js
+docker network connect database memebot
 docker start memebot
