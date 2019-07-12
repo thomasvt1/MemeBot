2e0cce5fb2119bf14579b49d50508fd58643fa18
Keanu <guestone63@gmail.com>
2019-07-12 12:54:45 +0100

    Fixed leaderboard
    My new setname code fucked it up a bit

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -9,16 +9,12 @@ exports.run = async (client, message, args) => {
 	const perPage = 20
 	const settings = message.guild ? await client.getSettings(message.guild) : await client.settings.findOne({ _id: "default" })
 	let isusername = true
-	let username = args[0] === undefined ? args[0] : args[0].replace(/^((\/|)u\/)/g, "")
-	const check = await client.api.getLink(client, message.author.id)
-	let user
-	if (username !== undefined) {
-		user = await client.api.getInvestorProfile(username).catch(err => {
-			if (err.statusCode !== 200) return message.channel.send(":exclamation: The meme.market API is currently down, please wait until it comes back up.")
-			client.logger.error(err.stack)
-		})
-	}
-	if (user === undefined && check) {
+	let user = await client.api.getInvestorProfile(username).catch(err => {
+		if (err.statusCode !== 200) return message.channel.send(":exclamation: The meme.market API is currently down, please wait until it comes back up.")
+		client.logger.error(err.stack)
+	})
+
+	if (user.id === 0 && check) {
 		user = await client.api.getInvestorProfile(check).catch(err => client.logger.error(err.stack))
 		username = user.name
 		isusername = false
