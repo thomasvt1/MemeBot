6155046cf837f43b4149b270696598fbe860b905
Keanu73 <guestone63@gmail.com>
2019-07-14 07:42:56 +0100

    Made investment watch more efficient

--- modules/websocket.js
+++ modules/websocket.js
@@ -9,6 +9,24 @@ module.exports = async (client, investment) => {
 	// note: investment watch should return something like this
 	// { submid: reddit_post_id, upvotes: reddit_post_upvotes, comments: reddit_comments, timediff: postedat (seconds), investments: investments, highinvestments: highinvestments, username: reddit_poster, famous: false }
 	//const famousmemers = ["organic_crystal_meth", "Hyp3r__", "SlothySurprise", "RegularNoodles", "JonathanTheZero", "TooEarlyForFlapjacks", "UncreativeFilth", "bleach_tastes_bad"]
+
+	const submission = await client.api.r.getSubmission(investment.submid).fetch().then((sub) => sub).catch(err => client.logger.error(err.stack))
+
+	const user = await client.api.getInvestorProfile(investment.username).catch(err => client.logger.error(err.stack))
+
+	const firm = await client.api.getFirmProfile(user.firm).catch(err => client.logger.error(err.stack))
+
+	//const famous = famousmemers.some(c => investment.username === c.toLowerCase()) ? "<:famousmemer:582821955489628166>" : ""
+
+	const firmemoji = firm.id !== 0 ? client.firmEmoji(firm.name) : ""
+
+	const timeposted = moment.duration(investment.timediff, "seconds").format("[**]m[**] [minutes] [ago], [**]s[**] [seconds] [ago]")
+
+	let msg = ""
+	msg += `This meme was posted ${timeposted} and should be profitable!\n`
+	msg += `There are currently **${investment.comments}** comments and **${investment.upvotes}** upvotes. I also count **${investment.investments}** investments and **${investment.highinvestments}** high investments.\n`
+	msg += `https://redd.it/${investment.submid}`
+
 	client.guilds.forEach(async (guild) => {
 		// investment watch channel will equal as channel id
 		const settings = await client.getSettings(guild)
@@ -22,23 +40,6 @@ module.exports = async (client, investment) => {
 		}
 
 		const mentioneveryone = settings.mentionEveryone === true ? "@everyone" : ""
-
-		const submission = await client.api.r.getSubmission(investment.submid).fetch().then((sub) => sub).catch(err => client.logger.error(err.stack))
-
-		const user = await client.api.getInvestorProfile(investment.username).catch(err => client.logger.error(err.stack))
-
-		const firm = await client.api.getFirmProfile(user.firm).catch(err => client.logger.error(err.stack))
-
-		//const famous = famousmemers.some(c => investment.username === c.toLowerCase()) ? "<:famousmemer:582821955489628166>" : ""
-
-		const firmemoji = firm.id !== 0 ? client.firmEmoji(firm.name) : ""
-
-		const timeposted = moment.duration(investment.timediff, "seconds").format("[**]m[**] [minutes] [ago], [**]s[**] [seconds] [ago]")
-
-		let msg = ""
-		msg += `This meme was posted ${timeposted} and should be profitable!\n`
-		msg += `There are currently **${investment.comments}** comments and **${investment.upvotes}** upvotes. I also count **${investment.investments}** investments and **${investment.highinvestments}** high investments.\n`
-		msg += `https://redd.it/${investment.submid}`
 
 		const investmentinfo = new RichEmbed()
 			.setAuthor("MemeBot Investment Watch", client.user.avatarURL, "https://github.com/thomasvt1/MemeBot")
